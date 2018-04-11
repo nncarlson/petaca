487aea26e6464d2e981c4b7ed10d75ee6cb06234
Neil Carlson <neil.n.carlson@gmail.com>
2018-04-11 10:37:44 -0600

    Some CMake file updates

--- cmake/FindYAJL.cmake
+++ cmake/FindYAJL.cmake
@@ -6,17 +6,29 @@
 #  YAJL_LIBRARIES
 #  YAJL_VERSION
 #
-# This module also defines the imported library target "yajl".  It is
-# generally enough to include "yajl" as a target link library; cmake
+# This module defines the imported library target YAJL::YAJL.  It is
+# generally enough to include YAJL::YAJL as a target link library; cmake
 # will automatically handle adding the appropriate compile include flags
 # and collection of link libraries.
 #
-# Set the variable CMAKE_PREFIX_PATH to provide a hint to the module for
-# where to find the library and header file.  This is searched before the
-# standard system locations.
+# To provide the module with a hint about where to find your YAJL installation
+# you have several options. You can include the root installation directory in
+# the setting of the CMAKE_PREFIX_PATH variable, or you can set the environment
+# variable YAJL_ROOT or the cmake variable YAJL_ROOT.
 
-find_path(YAJL_INCLUDE_DIR yajl/yajl_common.h)
-find_library(YAJL_LIBRARY NAMES yajl yajl_s) 
+if(NOT YAJL_ROOT)
+  set(YAJL_ROOT $ENV{YAJL_ROOT})
+endif()
+if(YAJL_ROOT)
+  set(yajl_search_opts NO_DEFAULT_PATH)
+else()
+  set(yajl_search_opts)
+endif()
+
+find_path(YAJL_INCLUDE_DIR NAMES yajl/yajl_common.h
+          HINTS ${YAJL_ROOT} ${yajl_search_opts} PATH_SUFFIXES include)
+find_library(YAJL_LIBRARY NAMES yajl yajl_s
+             HINTS ${YAJL_ROOT} ${yajl_search_opts} PATH_SUFFIXES lib)
 
 if(NOT YAJL_VERSION)
   if(YAJL_INCLUDE_DIR AND YAJL_LIBRARY)
@@ -37,16 +49,16 @@ endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(YAJL
-    REQUIRED_VARS YAJL_LIBRARY YAJL_INCLUDE_DIR 
+    REQUIRED_VARS YAJL_LIBRARY YAJL_INCLUDE_DIR
     VERSION_VAR YAJL_VERSION)
 
 if(YAJL_FOUND)
   set(YAJL_INCLUDE_DIRS ${YAJL_INCLUDE_DIR})
   set(YAJL_LIBRARIES ${YAJL_LIBRARY})
   mark_as_advanced(YAJL_INCLUDE_DIR YAJL_LIBRARY)
-  if(NOT TARGET yajl)
-    add_library(yajl UNKNOWN IMPORTED)
-    set_target_properties(yajl PROPERTIES
+  if(NOT TARGET YAJL::YAJL)
+    add_library(YAJL::YAJL UNKNOWN IMPORTED)
+    set_target_properties(YAJL::YAJL PROPERTIES
         IMPORTED_LOCATION "${YAJL_LIBRARY}"
         INTERFACE_INCLUDE_DIRECTORIES "${YAJL_INCLUDE_DIRS}"
         INTERFACE_LINK_LIBRARIES "${YAJL_LIBRARIES}")
